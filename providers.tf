28594541466a7c47baa03b91ebe2d3404de4d263
Natalia Matamala Izquierdo <natalia.matamala@babelgroup.com>
2023-06-22 23:37:07 +0200

    rehacer providers

--- providers.tf
+++ providers.tf
@@ -1,18 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">=4.36.0, <4.47.0, !=4.43.0"
-    }
-  }
-  required_version = "~>1.3.0"
-}
-
 provider "aws" {
-  region = "us-east-1"
-}
-
-provider "aws" {
-  region = "us-east-2"
-  alias  = "ohio"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region     = var.AWS_REGION
 }
